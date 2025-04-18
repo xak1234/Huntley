415887bc671e76a66983e1bd3544ae39bfc6da96
xak1234 <66489445+xak1234@users.noreply.github.com>
2025-04-18 23:36:42 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../')));
 
 // Load bot background from .docx
 let botBackground;
