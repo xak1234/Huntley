a9cac876a06a223a1ff3238b35f45db7b5cb354f
xak1234 <66489445+xak1234@users.noreply.github.com>
2025-04-19 09:30:04 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -98,7 +98,7 @@ app.get('/health', (req, res) => {
 
 // Fallback index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
