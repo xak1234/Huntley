deb257edc97acea942103a58ce28165cca2d8e63
xak1234 <66489445+xak1234@users.noreply.github.com>
2025-04-19 09:25:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../'))); // serve frontend from root folder
+app.use(express.static(path.join(__dirname, 'index.html'))); // serve frontend from root folder
 
 // Load personality from docx
 let botBackground = 'Default Huntley persona';
